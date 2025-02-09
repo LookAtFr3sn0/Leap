b7dea6584f8722a85efb3f7f54da494f96f7203c
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-02-09 08:11:44 +0100

    Implement draggable app window functionality

--- src/components/appWindow.component.vue
+++ src/components/appWindow.component.vue
@@ -1,12 +1,44 @@
 <script setup>
+import { usePointer } from '@vueuse/core';
+const { x, y } = usePointer();
 const props = defineProps(['app']);
-const appName =
-  props.app.charAt(0).toUpperCase() + props.app.slice(1)
+const appName = props.app.charAt(0).toUpperCase() + props.app.slice(1)
+
+function dragApp() {
+    const appWindow = document.getElementById('appWindow');
+    const appBar = document.getElementById('appBar');
+    appBar.style.cursor = 'grabbing';
+    let offsetX = appWindow.offsetLeft - x.value;
+    let offsetY = appWindow.offsetTop - y.value;
+
+    const onMouseMove = () => {
+        appWindow.style.left = offsetX + x.value + 'px';
+        appWindow.style.top = offsetY + y.value + 'px';
+        // keep the app window in the viewport
+        if (appWindow.offsetLeft < appWindow.offsetWidth/2) {
+            appWindow.style.left = appWindow.offsetWidth/2 + 'px';
+        }
+        if (appWindow.offsetTop < appWindow.offsetHeight/2) {
+            appWindow.style.top = appWindow.offsetHeight/2 + 'px';
+        }
+        if (appWindow.offsetLeft + appWindow.offsetWidth / 2 > appWindow.parentNode.clientWidth) {
+            appWindow.style.left = appWindow.parentNode.clientWidth - appWindow.offsetWidth/2 + 'px';
+        }
+        if (appWindow.offsetTop + appWindow.offsetHeight / 2 > appWindow.parentNode.clientHeight) {
+            appWindow.style.top = appWindow.parentNode.clientHeight - appWindow.offsetHeight/2 + 'px';
+        }
+    }
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', onMouseMove);
+        appBar.style.cursor = 'grab';
+    }, { once: true });
+}
 </script>
 
 <template>
-    <div class="bg-white max-w-4xl w-screen">
-        <div id="appBar" class="w-full flex justify-between">
+    <div id="appWindow" class="bg-white max-w-4xl w-screen">
+        <div id="appBar" class="w-full flex justify-between" @mousedown="dragApp()">
             <div class="px-2 py-0.5">{{ appName }}</div>
             <div class="flex *:cursor-pointer text-center *:aspect-square h-full absolute right-0">
                 <span class="material-symbols-outlined text-lg hover:bg-gray-200" @click="closeApp()">minimize</span>
