28a80f059fe23913e756894ea3d9a69a8018d462
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-02-09 20:39:55 +0100

    Remove click function

--- src/components/appWindow.component.vue
+++ src/components/appWindow.component.vue
@@ -45,8 +45,8 @@ function dragApp() {
         <div id="appBar" class="w-full flex justify-between" @mousedown="dragApp()">
             <div class="px-2 py-0.5">{{ appName }}</div>
             <div class="flex *:cursor-pointer text-center *:aspect-square h-full absolute right-0">
-                <span class="material-symbols-outlined text-lg hover:bg-gray-200" @click="closeApp()">minimize</span>
-                <span class="material-symbols-outlined text-lg hover:bg-gray-200" @click="closeApp()">crop_square</span>
+                <span class="material-symbols-outlined text-lg hover:bg-gray-200" @click="">minimize</span>
+                <span class="material-symbols-outlined text-lg hover:bg-gray-200" @click="">crop_square</span>
                 <span class="material-symbols-outlined text-lg hover:bg-red-600" @click="closeApp()">close</span>
             </div>
         </div>
