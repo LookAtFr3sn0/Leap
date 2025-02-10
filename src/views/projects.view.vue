81c46d68665be4e6f7e9936aac5cda2a138bb7a6
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-02-10 10:24:14 +0100

    Add project display to projects view

--- src/views/projects.view.vue
+++ src/views/projects.view.vue
@@ -1,2 +1,18 @@
+<script setup>
+</script>
+
 <template>
+    <div class="flex flex-col p-2">
+        <div class="pt-12 text-center text-xl">Projects</div>
+        <div class="grid grid-cols-2">
+            <RouterLink v-for="project in projects" class="p-2 m-2 rounded-lg shadow-md">
+                <div class="text-lg">{{ project.title }}</div>
+                <div class="text-sm">{{ project.description }}</div>
+                <div class="flex gap-4 justify-center text-white text-center h-10">
+                    <a :href="project.link" target="_blank" class="material-symbols-outlined bg-blue-400 p-1 text-2xl rounded-md aspect-square">link</a>
+                    <a :href="project.code" target="_blank" class="material-symbols-outlined bg-blue-400 p-1 text-2xl rounded-md aspect-square">code</a>
+                </div>
+            </RouterLink> 
+        </div>
+    </div>
 </template>
