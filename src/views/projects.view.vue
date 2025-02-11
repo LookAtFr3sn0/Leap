4427f1d9a40a939bc6456bd3acbdd8942c36c25f
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-02-12 00:34:49 +0100

    Update project link styling

--- src/views/projects.view.vue
+++ src/views/projects.view.vue
@@ -7,9 +7,9 @@ import projects from '../data/projects.data.json'
         <div class="pt-12 pb-2 text-center text-xl">My projects</div>
         <div class="grid grid-cols-2">
             <div v-for="project in projects" class="flex flex-col py-2 px-4 m-2 rounded-md bg-white shadow-md border border-[#D1D9E0]">
-                <RouterLink :to="project.page" class="text-lg dark:text-[#49AADE] font-medium hover:underline">{{ project.title }}</RouterLink>
+                <RouterLink :to="project.page" class="text-lg link font-medium mr-auto">{{ project.title }}</RouterLink>
                 <span class="text-sm my-2">{{ project.summary }}</span>
-                <span class="text-xs my-1">{{ project.technologies ? project.technologies.join(', ') : '' }}</span>
+                <span class="text-xs my-1">{{ project.technologies.join(' - ') }}</span>
             </div>
         </div>
     </div>
