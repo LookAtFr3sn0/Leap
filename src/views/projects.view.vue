3a8f2c79192fc448d68cdd53b435f9ec1314d8ad
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-02-11 17:39:52 +0100

    Reorder projects and rename description to summary

--- src/views/projects.view.vue
+++ src/views/projects.view.vue
@@ -8,9 +8,9 @@ import projects from '../data/projects.data.json'
         <div class="grid grid-cols-2">
             <div v-for="project in projects" class="flex flex-col py-2 px-4 m-2 rounded-md bg-white shadow-md border border-[#D1D9E0]">
                 <RouterLink :to="project.page" class="text-lg dark:text-[#49AADE] font-medium hover:underline">{{ project.title }}</RouterLink>
-                <span class="text-sm my-2">{{ project.description }}</span>
+                <span class="text-sm my-2">{{ project.summary }}</span>
                 <span class="text-xs my-1">{{ project.technologies ? project.technologies.join(', ') : '' }}</span>
-            </div> 
+            </div>
         </div>
     </div>
 </template>
