4b482ec6a1323e1ae48b726ffe0b6e719d53ad36
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-02-11 17:35:33 +0100

    Refactor projects view

--- src/views/projects.view.vue
+++ src/views/projects.view.vue
@@ -3,17 +3,14 @@ import projects from '../data/projects.data.json'
 </script>
 
 <template>
-    <div class="flex flex-col p-2">
-        <div class="pt-12 text-center text-xl">My projects</div>
+    <div class="flex flex-col p-2 h-full">
+        <div class="pt-12 pb-2 text-center text-xl">My projects</div>
         <div class="grid grid-cols-2">
-            <RouterLink v-for="project in projects" :to="project.page" class="p-2 m-2 rounded-lg shadow-md">
-                <div class="text-xl">{{ project.title }}</div>
-                <div class="text-sm py-2">{{ project.description }}</div>
-                <div class="flex gap-4 justify-center text-white text-center h-10">
-                    <a :href="project.link" class="material-symbols-outlined bg-blue-400 p-1 text-2xl rounded-md aspect-square">link</a>
-                    <a :href="project.code" class="material-symbols-outlined bg-blue-400 p-1 text-2xl rounded-md aspect-square">code</a>
-                </div>
-            </RouterLink> 
+            <div v-for="project in projects" class="flex flex-col py-2 px-4 m-2 rounded-md bg-white shadow-md border border-[#D1D9E0]">
+                <RouterLink :to="project.page" class="text-lg dark:text-[#49AADE] font-medium hover:underline">{{ project.title }}</RouterLink>
+                <span class="text-sm my-2">{{ project.description }}</span>
+                <span class="text-xs my-1">{{ project.technologies ? project.technologies.join(', ') : '' }}</span>
+            </div> 
         </div>
     </div>
 </template>
