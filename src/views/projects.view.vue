b0a8622bc606f0808613681b6827eb81c8fd976b
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-02-13 10:05:39 +0100

    Adjust padding in projects view

--- src/views/projects.view.vue
+++ src/views/projects.view.vue
@@ -4,7 +4,7 @@ import projects from '../data/projects.data.json'
 
 <template>
     <div class="flex flex-col p-2 h-full @container">
-        <div class="pt-12 pb-2 text-center text-xl">My projects</div>
+        <div class="pt-4 pb-2 text-center text-xl">My projects</div>
         <div class="grid grid-cols-1 @xl:grid-cols-2 @6xl:grid-cols-3">
             <div v-for="project in projects" class="flex flex-col py-2 px-4 m-2 rounded-md shadow-md border border-[#D1D9E0]">
                 <RouterLink :to="project.page" class="text-lg link hover:underline font-medium mr-auto">{{ project.title }}</RouterLink>
